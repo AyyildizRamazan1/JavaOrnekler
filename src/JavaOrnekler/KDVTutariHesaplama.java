34090ac76c1e2f2431e786aadc2ad0abb55d8c23
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2024-07-17 14:13:02 +0300

    KDV Tutarı Hesaplama

---
+++ src/JavaOrnekler/KDVTutariHesaplama.java
@@ -0,0 +1,20 @@
+package JavaOrnekler;
+import java.util.Scanner;
+
+public class KDVTutariHesaplama {
+	public static void main(String[] args) {
+		 Scanner scan = new Scanner(System.in);
+		 double price,total,kdvPrice,rate=18;
+		 
+		 System.out.print("Tutar Giriniz : ");
+		 price= scan.nextDouble();
+		 
+		 kdvPrice=price*(rate/100);
+		 total=kdvPrice+price;
+		 
+		 System.out.println("KDV Oranı : %"+(int)rate);
+		 System.out.println("KDV Tutarı : "+kdvPrice);
+		 System.out.println("KDV'li Tutarı : "+total);
+	}
+
+}
