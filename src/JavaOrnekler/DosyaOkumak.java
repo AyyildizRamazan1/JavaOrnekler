8522a59ee19c37ec65fb43215b65e795e822b22c
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2024-07-24 14:02:25 +0300

    Dosya içindekileri tamamen yazdırmak

--- src/JavaOrnekler/DosyaOkumak.java
+++ src/JavaOrnekler/DosyaOkumak.java
@@ -7,22 +7,29 @@ import java.io.IOException;
 public class DosyaOkumak {
 
 	public static void main(String[] args) {
-		FileInputStream fileInputStream=null;
-		
-		 try {
+		FileInputStream fileInputStream = null;
+
+		try {
 			fileInputStream = new FileInputStream("dosya.txt");
-			fileInputStream.skip(8);//başlanacak değeri gösteriyor
-			System.out.println((char)(fileInputStream.read()));
+			int deger;
+			String s = "";
+			while ((deger = fileInputStream.read()) != -1) {
+				s += (char) deger;
+			}
+			System.out.println(s);
+
+//			fileInputStream.skip(8);//başlanacak değeri gösteriyor
+//			System.out.println((char)(fileInputStream.read()));
 		} catch (FileNotFoundException e) {
 			System.out.println("Dosya Bulunamadı");
 		} catch (IOException e) {
-			System.out.println("Okunurken bir hata oluştu");			
-		}finally {
+			System.out.println("Okunurken bir hata oluştu");
+		} finally {
 			try {
-				if(fileInputStream != null) {
+				if (fileInputStream != null) {
 					fileInputStream.close();
 				}
-				
+
 			} catch (IOException e) {
 				System.out.println("Kapatılırken bir hata oluştu");
 			}
