9dcca5564ed5cef8eeec27e6a90e0d51d603c116
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2024-07-24 11:50:23 +0300

    Giriş Çıkış İşlemleri

--- src/JavaOrnekler/GirisCikisIslemleri.java
+++ src/JavaOrnekler/GirisCikisIslemleri.java
@@ -7,21 +7,34 @@ import java.io.IOException;
 public class GirisCikisIslemleri {
 
 	public static void main(String[] args) {
-		FileOutputStream fileOutPutStream= null;
+		FileOutputStream fileOutPutStream = null;
 		try {
-			fileOutPutStream = new FileOutputStream("dosya.txt");
+			fileOutPutStream = new FileOutputStream("dosya.txt", true); // true yazarsak eski yazdığımız verilerde kalır
+
+//			fileOutPutStream.write(85);
+//			fileOutPutStream.write(86);
+//
+//			byte[] dizi = { 66, 65, 66, 65 };
+//			fileOutPutStream.write(dizi);
+
+			String isim = "Ramazan Ayyıldız";
+			byte[] isimDizi = isim.getBytes();
+			fileOutPutStream.write(isimDizi);
+			
 		} catch (FileNotFoundException e) {
-			 System.out.println("Dosya bulunamadı");
-		}finally {
+			System.out.println("Dosya bulunamadı");
+		} catch (IOException e) {
+			System.out.println("Dosyaya yazılırken bir hata oluştu");
+		} finally {
 			try {
-				if(fileOutPutStream !=null) {
-					fileOutPutStream.close();	
+				if (fileOutPutStream != null) {
+					fileOutPutStream.close();
 				}
 			} catch (IOException e) {
 				System.out.println("Dosya kapatılırken bir hata ile karşılaşıldı!");
 			}
 		}
-		
+
 	}
 
 }
