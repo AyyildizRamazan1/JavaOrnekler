edc7d3bb1b33b8a31b6a78573e310de2c8ed10f6
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2024-07-26 15:44:22 +0300

    Return void kullanımı

--- src/JavaOrnekler/MetotOrn.java
+++ src/JavaOrnekler/MetotOrn.java
@@ -1,16 +1,16 @@
 package JavaOrnekler;
 
 public class MetotOrn {
-	
 	public static void main(String[] args) {
 		power(2, 3);
-	}
-	static void power(int sayi1, int sayi2) {
-		int result=1;
-		for(int i=1;i<=sayi2;i++) {
-			result *=sayi1;
-			}
-		System.out.println("Cevap: "+result);
-		}
-}
 
+	}
+
+	static void power(int sayi1, int sayi2) {
+		int result = 1;
+		for (int i = 1; i <= sayi2; i++) {
+			result *= sayi1;
+		}
+		System.out.println("Cevap: " + result);
+	}
+}
---
+++ src/JavaOrnekler/ReturnMetotlar.java
@@ -0,0 +1,19 @@
+package JavaOrnekler;
+
+public class ReturnMetotlar {
+
+	static int power(int a, int b) {
+		int result = 1;
+
+		for (int i = 1; i <= b; i++) {
+			result *= a;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int r = power(2, 3);
+		System.out.println(r);
+	}
+
+}
