9ce8b80090fbfc10e20ef121a39ead4b26e791c1
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2024-07-29 14:51:05 +0300

    Recursive Metotlar

---
+++ src/JavaOrnekler/RecursiveMetot.java
@@ -0,0 +1,23 @@
+package JavaOrnekler;
+
+public class RecursiveMetot {
+
+	static int f(int x) {
+		int toplam = 0;
+		for (int i = 1; i <= x; i++) {
+			toplam += i;
+		}
+		return toplam;
+	}
+
+	static int r(int x) {
+		if (x == 1) {
+			return 1;
+		}
+		return x + r(x - 1);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(r(10));
+	}
+}
