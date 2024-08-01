52a2957391e64537adc025113b9296352e34ad87
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2024-08-01 13:31:00 +0300

    Constructor(yapıcı) kullanımı ve Sınıf Metotları

--- src/JavaOrnekler/Siniflar.java
+++ src/JavaOrnekler/Siniflar.java
@@ -3,15 +3,26 @@ package JavaOrnekler;
 public class Siniflar {
 
 	public static void main(String[] args) {
-		HesapMakinesi makine1=new HesapMakinesi();
-		makine1.sayi1=10;
-		makine1.sayi2=20;
-		System.out.println(makine1.sayi1);
-		
-		HesapMakinesi makine2 = new HesapMakinesi();
-		makine2.sayi1=20;
-		System.out.println(makine2.sayi1);
+//		HesapMakinesi makine1=new HesapMakinesi();
+//		makine1.sayi1=10;
+//		makine1.sayi2=20;
+//		System.out.println(makine1.sayi1);
+//		
+//		HesapMakinesi makine2 = new HesapMakinesi();
+//		makine2.sayi1=20;
+//		System.out.println(makine2.sayi1);
 
+		HesapMakinesi m1=new HesapMakinesi(10,5,"siyah");
+		System.out.println(m1.renk);
+		System.out.println(m1.toplama());
+		m1.sayi1=8;
+		System.out.println(m1.cikarma());
+		
+		HesapMakinesi m2 = new HesapMakinesi(20,3,"mavi");
+		System.out.println(m1.renk);
+		System.out.println(m2.toplama());
+		
+		
 	}
 
 }
