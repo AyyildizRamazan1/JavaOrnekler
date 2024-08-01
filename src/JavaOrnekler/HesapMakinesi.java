fd2caca843feaf81f0b08dec2dc4abb331b22a89
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2024-08-01 11:11:17 +0300

    Sınıftan nesne türetme

---
+++ src/JavaOrnekler/HesapMakinesi.java
@@ -0,0 +1,6 @@
+package JavaOrnekler;
+
+public class HesapMakinesi {
+	public int sayi1;
+	public int sayi2;	
+}
---
+++ src/JavaOrnekler/Siniflar.java
@@ -0,0 +1,17 @@
+package JavaOrnekler;
+
+public class Siniflar {
+
+	public static void main(String[] args) {
+		HesapMakinesi makine1=new HesapMakinesi();
+		makine1.sayi1=10;
+		makine1.sayi2=20;
+		System.out.println(makine1.sayi1);
+		
+		HesapMakinesi makine2 = new HesapMakinesi();
+		makine2.sayi1=20;
+		System.out.println(makine2.sayi1);
+
+	}
+
+}
