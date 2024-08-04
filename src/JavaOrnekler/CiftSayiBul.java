1e51ef779bf50bee10f088e9e777f0899e9f8e78
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2024-08-04 22:46:13 +0300

    4 ve 5 e tam bölünen sayıların listesini oluşturup ortalaması bulunuyor

--- src/JavaOrnekler/CiftSayiBul.java
+++ src/JavaOrnekler/CiftSayiBul.java
@@ -1,5 +1,7 @@
 package JavaOrnekler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class CiftSayiBul {
@@ -8,13 +10,24 @@ public class CiftSayiBul {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Lütfen bir sayı giriniz: ");
 		int sayi = scan.nextInt();
+		int toplam = 0;
+		List<Integer> sayilar = new ArrayList<>();
 
 		for (int i = 1; i <= sayi; i++) {
-			if (i % 2 == 0) {
-				System.out.println(i);
+			if (i % 20 == 0) {
+				sayilar.add(i);
+				toplam += i; // Liste elemanları toplama işlemi
 			}
+
 		}
 
+		System.out.println("Dizideki elemanlar: " + sayilar);
+
+		System.out.println("Dizideki eleman sayısı: " + sayilar.stream().count()); // Listenin elaman sayısını görme
+
+		System.out.println("Dizideki elemanların toplamı: " + toplam); // Listedeki elemanların toplamı
+		int ort = (int) sayilar.stream().count();
+		System.out.println("Dizinin ortalaması: " + toplam / ort);
 	}
 
 }
