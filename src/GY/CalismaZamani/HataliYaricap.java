1876662a60102d365dc1b4c4bcc36c9ab6de872e
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2025-08-28 14:25:21 +0300

    Hatalı Yarıçap

---
+++ src/GY/CalismaZamani/HataliYaricap.java
@@ -0,0 +1,10 @@
+package GY.CalismaZamani;
+
+public class HataliYaricap extends RuntimeException {
+
+    private double yaricap;
+
+    public HataliYaricap(double yaricap) {
+        super("Yaricap " + yaricap + " olamaz!");
+    }
+}
