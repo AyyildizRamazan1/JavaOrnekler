6dc68b7260856ab971054cd487982de1ad65fcc6
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2025-09-03 12:43:18 +0300

    Sınıf Üyeleri 1

---
+++ src/GY/OOP/UyeDegiskenler/Bisiklet.java
@@ -0,0 +1,14 @@
+package GY.OOP.UyeDegiskenler;
+
+public class Bisiklet {
+    int vites;
+    int hiz;
+
+    static int bisikletSayisi = 0;
+
+    void bilgileriYazdir() {
+        System.out.println(vites);
+        System.out.println(hiz);
+        System.out.println(Bisiklet.bisikletSayisi);
+    }
+}
