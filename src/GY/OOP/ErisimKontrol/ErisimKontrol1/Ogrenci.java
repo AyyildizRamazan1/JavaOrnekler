d27f95ead4e9394ca191b43adb91ba339e200753
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2025-09-15 16:10:50 +0300

    Erişim Kontrolü-1

---
+++ src/GY/OOP/ErisimKontrol/ErisimKontrol1/Ogrenci.java
@@ -0,0 +1,12 @@
+package GY.OOP.ErisimKontrol.ErisimKontrol1;
+
+public class Ogrenci {
+
+    private int numara;
+    private String ad;
+    private String soyad;
+
+    void numarayiYazdir() {
+        System.out.println(numara);
+    }
+}
